c7ab96582d6c9f77a5266f19574271a11a0fe823
PourthieAlexis <alexispourthie@gmail.com>
2024-02-13 11:37:15 +0100

    init project & header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
+import LogoCook from "./../assets/images/logo.png";
+
+function Header() {
+  return (
+    <header>
+      <div className="logo-menu">
+        <FontAwesomeIcon icon={faBars} />
+        <img src={LogoCook} alt="logo Cook Formation CDA" />
+      </div>
+      <div className="groups-buttons">
+        <button className="cart">
+          <FontAwesomeIcon icon={faBasketShopping} /> Panier
+        </button>
+        <button className="login">Connexion</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
